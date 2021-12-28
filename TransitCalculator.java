a9e41c798bf88f80c12bfb88b58cda2b524a79e0
Cole Cianflone <colecianflone123@gmail.com>
2021-12-28 12:36:49 -0600

    Update TransitCalculator.java

--- TransitCalculator.java
+++ TransitCalculator.java
@@ -10,11 +10,11 @@ public class TransitCalculator
 {
 	// fields
 	int numberOfDays,
-		numberOfRides;
+		  numberOfRides;
 
 	String[] fareOptions = {"Pay-per-ride (single ride): ",
-							"7-day Unlimited Rides: ",
-							"30-day Unlimted Rides: "};
+							            "7-day Unlimited Rides: ",
+							            "30-day Unlimted Rides: "};
 
 	double[] farePrices = {2.75, 33.00, 127.00};
 
@@ -26,46 +26,45 @@ public class TransitCalculator
 	}
 
 	// methods
-    double unlimited7Price()
-    {
-      double multipler = Math.ceil((double) numberOfDays / 7);
-      return (double)( farePrices[1] * multipler ) / numberOfRides;
-    }
+  double unlimited7Price()
+  {
+		double multipler = Math.ceil((double) numberOfDays / 7);
+    return (double)( farePrices[1] * multipler ) / numberOfRides;
+  }
 
-    double unlimited30Price()
-    {
-      double multipler = Math.ceil((double) numberOfDays / 30);
-      return ( farePrices[2] * multipler ) / numberOfRides;
-
-    }
+  double unlimited30Price()
+  {
+    double multipler = Math.ceil((double) numberOfDays / 30);
+		return ( farePrices[2] * multipler ) / numberOfRides;
+  }
 
 	double[] getRidePrices()
 	{
 		double[] prices = {farePrices[0] * numberOfDays,
-						   unlimited7Price(),
-						   farePrices[2]};
+						           unlimited7Price(),
+						           farePrices[2]};
 		return prices;
 	}
 
-    String getBestFare()
-    {
-      double[] ridePrices = getRidePrices();
-      int cheapestIndex = 0;
+  String getBestFare()
+  {
+    double[] ridePrices = getRidePrices();
+    int cheapestIndex = 0;
 
-      for (int i = 0; i < ridePrices.length; i++)
-      {
-        if (ridePrices[i] < ridePrices[cheapestIndex])
-          cheapestIndex = i;
-      }
+    for (int i = 0; i < ridePrices.length; i++)
+    {
+      if (ridePrices[i] < ridePrices[cheapestIndex])
+        cheapestIndex = i;
+    }
       return "You should get the " + fareOptions[cheapestIndex] +
-      		 " option at $" + ridePrices[cheapestIndex] + " per ride.";
+      		   " option at $" + ridePrices[cheapestIndex] + " per ride.";
     }
 
 	// main testing
 	public static void main(String[] args)
 	{
 		int myRides = 14,
-			myDays = 7;
+			  myDays = 7;
 
 		Scanner sc = new Scanner(System.in);
 
