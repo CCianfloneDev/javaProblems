0378a831cfe75d1ebffb321ab19abcfcae447a90
Cole Cianflone <colecianflone123@gmail.com>
2021-12-28 12:23:57 -0600

    Update TransitCalculator.java

--- TransitCalculator.java
+++ TransitCalculator.java
@@ -13,8 +13,8 @@ public class TransitCalculator
 		  numberOfRides;
 
 	String[] fareOptions = {"Pay-per-ride (single ride): ",
-							            "7-day Unlimited Rides: ",
-							            "30-day Unlimted Rides: "};
+													"7-day Unlimited Rides: ",
+													"30-day Unlimted Rides: "};
 
 	double[] farePrices = {2.75, 33.00, 127.00};
 
