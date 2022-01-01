ad32911a8170990664fd6af23ee66b6ed5324c33
Cole Cianflone <colecianflone123@gmail.com>
2021-12-31 18:04:23 -0600

    Added javadoc to Droid.java

--- Droid.java
+++ Droid.java
@@ -1,39 +1,61 @@
 package Java;
-
 /** Simple program to simulate a droid. Codecademy Project: Learn Java - Build A Droid
  * Purpose: Demonstrate understanding of invoking and declaring methods
- * Author: Cole Cianflone
+ *
+ * @Author: Cole Cianflone
  * Date: Dec 22nd, 2021
  */
-
 public class Droid {
 
     int batteryLevel;
     String name;
 
+    /**
+     * Droid constructor
+     * @param name
+     */
     public Droid(String name)
     {
       this.name = name;
       batteryLevel = 100;
     }
 
+    /**
+     * Perform task method, prints out name + " is performing: " + task,
+     * and depletes batteryLevel by 10
+     * @param task
+     */
     public void performTask(String task)
     {
       System.out.println(name + " is performing: " + task);
       batteryLevel -= 10;
     }
-
+    
+    /**
+     * Returns energyReport, prints out name + "'s battery is currently at: " + batteryLevel.
+     * @return name + "'s battery is currently at: " + batteryLevel
+     */
     public String energyReport()
     {
       return name + "'s battery is currently at: " + batteryLevel;
     }
-
+    
+    /**
+     * Perform energyTransfer, transfers power to another Droid object,
+     * and depletes amount of power transferd from invoked Droid object.
+     * @param power
+     * @param Droid 
+     */
     public void energyTransfer(int power, Droid transferTo)
     {
       batteryLevel -= power;
       transferTo.batteryLevel += power;
     }
-
+    
+    /**
+     * Returns strin representation of Droid object.
+     * @return "Hello, I'm the droid: " + name
+     */
     public String toString()
     {
       return "Hello, I'm the droid: " + name;
@@ -41,9 +63,11 @@ public class Droid {
 
     public static void main(String[] args)
     {
+      // 2 new droid objects
       Droid droid01 = new Droid("Codey");
       Droid droid02 = new Droid("Cole");
 
+      // testing methods
       System.out.println(droid01);
       droid01.performTask("Mopping floor");
       System.out.println(droid01.energyReport());
